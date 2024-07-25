830af95fd2326337f58b20dd1e5754bf2f82a771
shahzaibdz <shahzaib2005dz@gmail.com>
2024-07-25 17:27:03 +0500

    MindLink update DB

--- app/src/main/java/com/example/mindlink/DB.java
+++ app/src/main/java/com/example/mindlink/DB.java
@@ -1,10 +1,15 @@
 package com.example.mindlink;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB extends SQLiteOpenHelper {
     private static DB instance;
@@ -22,11 +27,62 @@ public class DB extends SQLiteOpenHelper {
         }
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+     db.execSQL(Note.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+      if(oldVersion != newVersion){
+          db.execSQL(Note.DROP_TABLE);
+          db.execSQL(Note.CREATE_TABLE);
+      }
     }
+    public boolean insertNote(Note note){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Note.COL_TITLE,note.getTitle());
+        contentValues.put(Note.COL_DESCRIPTION,note.getDescription());
+        contentValues.put(Note.COL_TIME,note.getTime());
+        long rowId;
+        try {
+            rowId = db.insert(Note.TABLE_NAME,null,contentValues);
+        }catch (Exception ex){
+            return false;
+        }
+        return rowId!= -1;
+    }
+
+    public boolean updateNote(Note note){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Note.COL_TITLE,note.getTitle());
+        contentValues.put(Note.COL_DESCRIPTION,note.getDescription());
+        contentValues.put(Note.COL_TIME,note.getTime());
+        long rowId;
+        try {
+            rowId = db.update(Note.TABLE_NAME,contentValues,Note.COL_ID+" = ?",new String[note.getId()]);
+        }catch (Exception ex){
+            return false;
+        }
+        return rowId != -1;
+    }
+    public boolean deleteNote(Note note){
+        SQLiteDatabase db = getWritableDatabase();
+
+        long rowId;
+        try {
+            rowId = db.delete(Note.TABLE_NAME,Note.COL_ID+" = ?",new String[note.getId()]);
+        }catch (Exception ex){
+            return false;
+        }
+        return rowId != -1;
+    }
+
+    public List<Note> fetchNotes(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(Note.SELECT_ALL_NOTES,null);
+        List<Note> notes = new ArrayList<>();
+        return notes;
+    }
+
 }
--- app/src/main/java/com/example/mindlink/Note.java
+++ app/src/main/java/com/example/mindlink/Note.java
@@ -6,21 +6,28 @@ public class Note {
     public String title;
     public String description;
     public String time;
+    public int id = 0;
 
     public static final String TABLE_NAME = "MindLink";
     public static final String COL_TITLE = "Title";
     public static final String COL_DESCRIPTION = "Description";
     public static final String COL_TIME = "Time";
+    public static final String COL_ID = "id";
 
+
+    public static final String CREATE_TABLE = String.format("CREATE TABLE IF NOT EXISTS %s(%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT,%s TEXT,%s TEXT)",TABLE_NAME,COL_ID,COL_TITLE,COL_DESCRIPTION,COL_TIME);
+    public static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;
+    public static final String SELECT_ALL_NOTES ="SELECT * FROM "+TABLE_NAME;
 
 
     public Note() {
     }
 
-    public Note(String title, String description, String time) {
+    public Note(String title, String description, String time , int id) {
         this.title = title;
         this.description = description;
         this.time = time;
+        this.id = id;
     }
 
     public String getTitle() {
@@ -47,17 +54,25 @@ public class Note {
         this.time = time;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Note note = (Note) o;
-        return Objects.equals(title, note.title) && Objects.equals(description, note.description) && Objects.equals(time, note.time);
+        return id == note.id && Objects.equals(title, note.title) && Objects.equals(description, note.description) && Objects.equals(time, note.time);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, description, time);
+        return Objects.hash(title, description, time, id);
     }
 
     @Override
@@ -66,6 +81,7 @@ public class Note {
                 "title='" + title + '\'' +
                 ", description='" + description + '\'' +
                 ", time='" + time + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
